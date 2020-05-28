3cafaf6125c11ffd2dd90b64dd20f66d127a278c
Ludovic Ortega <mastership@hotmail.fr>
2020-05-28 02:33:38 +0200

    Fix vue config

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@ module.exports = {
   css: {
     loaderOptions: {
       less: {
-        modifyVars: {
-          "primary-color": "#008080",
-          "link-color": "#008080",
-          "border-radius-base": "2px",
-        },
-        javascriptEnabled: true
+        lessOptions: {
+          modifyVars: {
+            "primary-color": "#008080",
+            "link-color": "#008080",
+            "border-radius-base": "2px",
+          },
+          javascriptEnabled: true
+        }
       }
     }
   },
